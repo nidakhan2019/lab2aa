9e9fef05443b7685eac9361519159e7d10ef3fee
csmalaika <malaika2019@namal.edu.pk>
2021-10-03 01:24:36 -0700

    mss

---
+++ cal.py
@@ -0,0 +1,17 @@
+multiply (x,y):
+    return x * y
+print (multiply (5,6))
+
+def squareRoot(x):
+	return x ** 0.5
+
+x = squareRoot(10)
+print(x)
+def add(x,y):
+	return (x+y)
+m=add(6,9)
+print(m)
+def sub (x,y)
+    return x-y
+a = sub(8,14)
+print(a)
