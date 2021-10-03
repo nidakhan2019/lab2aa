baf9e8c331a89a6577165dc2c2458239d2373094
kiran-shahid <kirans2019@namal.edu.pk>
2021-10-03 14:46:22 +0500

    4_commit

---
+++ data4.py
@@ -0,0 +1,22 @@
+multiply (x,y):
+    return x * y
+print (multiply (5,6))
+
+def squareRoot(x):
+	return x ** 0.5
+
+x = squareRoot(10)
+print(x)
+def add(x,y):
+	return (x+y)
+m=add(6,9)
+print(m)
+def sub (x,y)
+    return x-y
+a = sub(8,14)
+print(a)
+
+def divide (x,y):
+	return x / y
+
+print(12,7)
